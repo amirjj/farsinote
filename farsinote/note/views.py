a3c497e89013ea5a341931b52a8a153a307ed813
Ubuntu 18.4 <amir.jamshidijam@gmail.com>
2021-03-04 17:21:28 +0330

    feat: Profile/signals added

--- farsinote/note/views.py
+++ farsinote/note/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.views import generic
 import datetime
