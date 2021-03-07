c4134804be3a58f79706f14395a769db8479d4b9
Ubuntu 18.4 <amir.jamshidijam@gmail.com>
2021-03-07 17:04:40 +0330

    feat: delete added

--- farsinote/note/urls.py
+++ farsinote/note/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from .views import NoteListView, NoteDetailView, NoteCreateView
+from .views import (
+    NoteListView,
+    NoteDetailView,
+    NoteCreateView,
+    NoteUpdateView,
+    NoteDeleteView
+)
 
 urlpatterns = [
     path('', NoteListView.as_view(), name='index'),
     path('create/', NoteCreateView.as_view(), name='create'),
     path('<int:pk>/', NoteDetailView.as_view(), name='detail'),
+    path('<int:pk>/update/', NoteUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', NoteDeleteView.as_view(), name='delete'),
     # path('new/', new, name='new')
 ]
--- farsinote/note/views.py
+++ farsinote/note/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpResponse
 from django.views.generic import (
     ListView,
     DetailView,
     CreateView,
-    DeleteView
+    DeleteView,
+    UpdateView
 )
 import datetime
 from .models import Note
 
 
-class NoteListView(ListView):
+class NoteListView(LoginRequiredMixin, ListView):
     model = Note
     template_name = 'note/index.html'
     context_object_name = 'note_list'
@@ -22,16 +23,36 @@ def create(request):
     return HttpResponse('this is the create note form')
 
 
-class NoteDetailView(DetailView):
+class NoteDetailView(LoginRequiredMixin, DetailView):
     model = Note
     template_name = 'note/detail.html'
     context_object_name = 'note'
 
 
-class NoteCreateView(CreateView):
+class NoteDeleteView(LoginRequiredMixin, DeleteView):
+    model = Note
+
+    success_url = "/note/"
+    # template_name = 'note/detail.html'
+    # context_object_name = 'note'
+
+
+
+class NoteCreateView(LoginRequiredMixin, CreateView):
     model = Note
     fields = ['title', 'body']
 
+
+# class NoteUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+class NoteUpdateView(LoginRequiredMixin, UpdateView):
+    model = Note
+    fields = ['title', 'body']
+
+    # def test_func(self):
+    #     note = self.get_object()
+    #     if note.author == self.request.user:
+    #         return True
+    #     return False
 
 
 # def new(request):
